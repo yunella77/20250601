718daa83fd7600528f39171a8ce57bc0b454021b
yunella77 <413730374@o365.tku.edu.tw>
2025-06-01 15:48:59 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,11 +2,22 @@ let video;
 
 function setup() {
   createCanvas(640, 480);
+  // 將畫布置中
+  let canvas = createCanvas(640, 480);
+  canvas.parent(document.body);
+  canvas.style('display', 'block');
+  canvas.style('margin', 'auto');
+  canvas.position(
+    (windowWidth - width) / 2,
+    (windowHeight - height) / 2
+  );
+
   video = createCapture(VIDEO);
   video.size(width, height);
-  video.hide(); // 隱藏預設的 video 標籤
+  video.hide();
 }
 
 function draw() {
+  background(220);
   image(video, 0, 0, width, height);
 }
