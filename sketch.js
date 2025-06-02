e53c6cd7e05d5e0b8f6c39fbf5e3bf45da0b8f70
yunella77 <413730374@o365.tku.edu.tw>
2025-06-02 15:21:58 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -20,23 +20,5 @@ function modelReady() {
 
 function draw() {
   background(200);
-  console.log(video);
   image(video, 0, 0, width, height);
-  drawHands();
-}
-
-function drawHands() {
-  for (let i = 0; i < predictions.length; i++) {
-    const hand = predictions[i];
-    // palmBase 是手掌中心點
-    const palm = hand.annotations.palmBase[0];
-    fill(255, 0, 0);
-    noStroke();
-    ellipse(palm[0], palm[1], 20, 20);
-
-    // 顯示左右手資訊
-    fill(0);
-    textSize(16);
-    text(hand.handInViewConfidence > 0.8 ? (hand.handedness === "Left" ? "左手" : "右手") : "", palm[0] + 10, palm[1]);
-  }
 }
