ab2ae5b77e6f28d2bb15985ca2d30499af3e00e6
yunella77 <413730374@o365.tku.edu.tw>
2025-06-02 16:08:11 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,13 +1,36 @@
 let video;
+let facemesh;
+let predictions = [];
 
 function setup() {
   createCanvas(640, 480);
   video = createCapture(VIDEO);
-  video.size(640, 480);
-  // video.hide(); // 先註解這行
+  video.size(width, height);
+  video.hide();
+
+  facemesh = ml5.facemesh(video, modelReady);
+  facemesh.on("predict", results => {
+    predictions = results;
+  });
+}
+
+function modelReady() {
+  console.log("Facemesh model ready!");
 }
 
 function draw() {
-  background(200);
   image(video, 0, 0, width, height);
+
+  if (predictions.length > 0) {
+    const keypoints = predictions[0].scaledMesh;
+
+    // 範例：畫出鼻子與左眼、右眼
+    fill(0, 255, 0);
+    noStroke();
+
+    // 鼻尖：168，左眼：33，右眼：263
+    ellipse(keypoints[168][0], keypoints[168][1], 10); // 鼻尖
+    ellipse(keypoints[33][0], keypoints[33][1], 10);   // 左眼
+    ellipse(keypoints[263][0], keypoints[263][1], 10); // 右眼
+  }
 }
