42fc93917d1ba5f1bbbb532bee4f01707bdacc9c
yunella77 <413730374@o365.tku.edu.tw>
2025-06-02 15:23:39 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,21 +1,10 @@
 let video;
-let handpose;
-let predictions = [];
 
 function setup() {
   createCanvas(640, 480);
   video = createCapture(VIDEO);
   video.size(640, 480);
   // video.hide(); // 先註解這行
-
-  handpose = ml5.handpose(video, modelReady);
-  handpose.on("predict", results => {
-    predictions = results;
-  });
-}
-
-function modelReady() {
-  console.log("Handpose model ready!");
 }
 
 function draw() {
